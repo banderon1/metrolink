09cd4b91a33e4398e49bd84dc65652c7777a82ce
Brian Anderson <banderon1@gmail.com>
2017-10-01 17:24:36 -0700

    Update js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 //setInterval(function(){
 setTimeout(function(){
-    $('.box-1').toggleClass('magictime tinLeftIn');
-    $('.box-2').toggleClass('magictime tinRightIn');
-    $('.box-3').toggleClass('magictime tinUpIn');
-    $('.box-4').toggleClass('magictime tinDownIn');
+    $('.box-1').addClass('magictime rotateLeft');
+    $('.box-2').addClass('magictime rotateRight');
+    $('.box-3').addClass('magictime rotateUp');
+    $('.box-4').addClass('magictime rotateDown');
 }, 2000);
+setTimeout(function(){
+    $('.box-1').removeClass('magictime rotateLeft');
+    $('.box-2').removeClass('magictime rotateRight');
+    $('.box-3').removeClass('magictime rotateUp');
+    $('.box-4').removeClass('magictime rotateDown');
+}, 4000);
