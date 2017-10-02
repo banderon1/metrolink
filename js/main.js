465852432138e65a1e6a54830029d0671e6d4f8e
Brian Anderson <brian.anderson@carpe.io>
2017-10-01 17:21:35 -0700

    metrolink initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//setInterval(function(){
+setTimeout(function(){
+    $('.box-1').toggleClass('magictime tinLeftIn');
+    $('.box-2').toggleClass('magictime tinRightIn');
+    $('.box-3').toggleClass('magictime tinUpIn');
+    $('.box-4').toggleClass('magictime tinDownIn');
+}, 2000);
