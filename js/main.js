5c94f3554d47d8015dd9a27da8849ce40f7d8666
Brian Anderson <brian.anderson@carpe.io>
2017-10-05 21:17:35 -0700

    add footer

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(function(){
     setTimeout(function(){
         page = page + 1;
         showNextMontage(page);
-    }, 8000);
+    }, 3000);
 });
 $('.montage').click(function(){
     var page = parseInt($(this).attr('id').replace(/\D/g,''));
@@ -17,11 +17,10 @@ function showNextMontage(page) {
     if(page >= 2) nextPage = 1;
     var animations = ['', 'perspectiveLeftReturn', 'swap'];
     var nextAnimation = animations[nextPage];
-    console.log(nextAnimation);
     $('#page' + nextPage + ' .row div').addClass('magictime ' + nextAnimation);
     $('#page' + page).hide();
     $('#page' + nextPage).show();
     setTimeout(function(){
         $('#page' + nextPage + ' .row div').removeClass('magictime ' + nextAnimation);
-    }, 1000);
+    }, 3000);
 }
