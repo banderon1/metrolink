e157ff83fa6ab1dccfb9572ddd774a1d3234f83e
Brian Anderson <brian.anderson@carpe.io>
2017-10-09 19:28:19 -0700

    add videos

--- js/main.js
+++ js/main.js
@@ -2,12 +2,39 @@
 var timeoutMontage;
 $(function(){
     showNextMontage(0);
-});
 
-$('.montage').click(function(){
-    var page = parseInt($(this).attr('id').replace(/\D/g,''));
-    clearTimeout(timeoutMontage);
-    showNextMontage(page);
+    $('.montage').click(function(){
+        var page = parseInt($(this).attr('id').replace(/\D/g,''));
+        clearTimeout(timeoutMontage);
+        showNextMontage(page);
+    });
+
+    $('.video').click(function(e){
+        e.stopPropagation();
+        clearTimeout(timeoutMontage);
+        videos = [
+            '',
+            '07-13-17_National Icecream Month (Ben & Jerry\'s)_B-Roll',
+            'How to Ride Metrolink',
+            'Metrolink 25 Years - Draft 5',
+            'Metrolink Destination Series  Beaches',
+            'Metrolink Destination Series  Hollywood',
+            'Metrolink Destination Series  Olvera Street',
+            'Metrolink Destinations - San Juan Capistrano',
+            'Metrolink_PTC'
+        ];
+        $('#videoModal').modal();
+        var myVideo = document.getElementById("videoPopup");
+        var videoId = parseInt($(this).attr('id').replace(/\D/g,''));
+        myVideo.src = 'img/videos/' + encodeURIComponent(videos[videoId]) + '.mp4';
+        myVideo.play();
+    });
+
+    //pause the video if the popup is closed
+    $('#videoModal').on('hidden.bs.modal', function (e) {
+        var myVideo = document.getElementById("videoPopup");
+        myVideo.pause();
+    });
 });
 
 function showNextMontage(page) {
@@ -24,10 +51,3 @@ function showNextMontage(page) {
         showNextMontage(nextPage);
     }, 8000);
 }
-
-$('.video').click(function(e){
-    e.stopPropagation();
-    clearTimeout(timeoutMontage);
-    var videoId = parseInt($(this).attr('id').replace(/\D/g,''));
-    console.log('video click' + videoId);
-});
